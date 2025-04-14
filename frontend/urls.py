8dc548c6487ecc250c50f89b65337cb4ab58db4d
ReZoS <styanev89@gmail.com>
2025-04-14 14:02:55 +0300

    cookie policy

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import home
+from django.views.generic import TemplateView
+
 
 app_name = 'home'
 
 urlpatterns = [
     path('', home, name='home'),
+    path("privacy-policy/", TemplateView.as_view(template_name="privacy_policy.html"), name="privacy_policy"),
+
 ]
